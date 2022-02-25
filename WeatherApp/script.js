052eb3b93decaab654e267f49aa8f48a0723a76f
Bartosz <bar.brodowski@gmail.com>
2022-02-25 19:25:54 +0100

    Changing class names for easier reading

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -1,1 +1,15 @@
 // 3f93a2a634d9395fdeb55da15ee519b2
+
+const inputCard = document.querySelector(".input-card");
+const inputBox = inputCard.querySelector(".city-input-box");
+
+let api;
+
+function getInput() {
+    if (inputBox.value != "") {
+    }
+}
+function getWeather(city) {
+    api =
+        "api.openweathermap.org/data/2.5/weather?q=${city}&appid=3f93a2a634d9395fdeb55da15ee519b2";
+}
