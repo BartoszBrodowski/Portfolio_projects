565f93c113b0eb4719f2e12cada606e71d18978c
Bartosz <bar.brodowski@gmail.com>
2022-03-24 09:10:41 +0100

    Working inputs, TODO add validation check

--- Sudoku/sudoku/src/App.js
+++ Sudoku/sudoku/src/App.js
@@ -1,29 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./components/Board.js";
-
-const board = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-];
-
-document.getElementsByClassName("nodeInput");
-
-const checkNumber = (nodeInput) => {
-    if (nodeInput.value < 0 || nodeInput.value > 9) {
-        console.log("mieści się");
-        return true;
-    } else {
-        console.log("nie mieści się");
-        return false;
-    }
-};
 
 function App() {
     return <Board />;
--- Sudoku/sudoku/src/components/Board.js
+++ Sudoku/sudoku/src/components/Board.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+
+const initialBoard = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1],
+];
 
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+const node = document.getElementsByClassName("nodeInput");
 
 const Board = () => {
+    // Get deep variable of the board
+    const deepVariable = (board) => {
+        return JSON.parse(JSON.stringify(board));
+    };
+
+    const [board, setBoard] = useState(deepVariable(initialBoard));
+
+    const changeNumber = (event, row, column) => {
+        console.log(parseInt(event));
+        const inputValue = parseInt(event.target.value) || 0,
+            playersBoard = deepVariable(board);
+        // Czytamy wartości od 1 do 9 (tylko takie mamy w Sudoku) oraz 0 jako wartość pustego pola
+        if (inputValue === 0 || (inputValue >= 1 && inputValue <= 9)) {
+            playersBoard[row][column] = inputValue;
+        }
+        setBoard(playersBoard);
+        // isValid()
+    };
+
     return (
         <div className="sudoku">
             <div className="header">Sudoku!</div>
@@ -14,7 +46,17 @@ const Board = () => {
                                 {numbers.map((column, columnIndex) => {
                                     return (
                                         <td key={columnIndex}>
-                                            <input className="nodeInput" />
+                                            <input
+                                                value={
+                                                    board[row][column] === 0
+                                                        ? ""
+                                                        : board[row][column]
+                                                }
+                                                className="nodeInput"
+                                                onChange={(e) =>
+                                                    changeNumber(e, row, column)
+                                                }
+                                            />
                                         </td>
                                     );
                                 })}
